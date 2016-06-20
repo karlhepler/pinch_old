e99efeb42a4c05970f2ccab3ddd942f593049e4f
Karl Hepler <khepler@brandmovers.com>
2016-06-19 22:40:19 -0400

    A quick note on line commodities

--- source/app/Models/Line/Abstracts/Line.php
+++ source/app/Models/Line/Abstracts/Line.php
@@ -13,6 +13,12 @@ use App\Models\Line\Traits\Relationships;
 /**
  * A line is a single line of itemization of an account
  * in a transaction and in the account's ledger.
+ * 
+ * In the future, I might add an optional field for commodity,
+ * which will allow the user to keep track of the specific thing
+ * bought/sold on this line. This might allow the system to catch
+ * wind of it, figure out if it is an asset, and recommend a
+ * sinking fund for it.
  */
 abstract class Line extends Model
 {
